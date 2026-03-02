48adfe38413344f402fa721adde8abfa519ca625
Muhammad Amien <muhammadamien23@gmail.com>
2026-03-02 15:22:07 +0700

    feat: setup and test ping redis

---
+++ redis_test.go
@@ -0,0 +1,29 @@
+package belajar_golang_redis
+
+import (
+	"context"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+)
+
+var client = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+	DB:   0,
+})
+
+func TestConnection(t *testing.T) {
+	assert.NotNil(t, client)
+
+	//err := client.Close()
+	//assert.Nil(t, err)
+}
+
+var ctx = context.Background()
+
+func TestPing(t *testing.T) {
+	result, err := client.Ping(ctx).Result()
+	assert.Nil(t, err)
+	assert.NotNil(t, "PONG", result)
+}
